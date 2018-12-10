6bd540a71b4b106ffaa5b847208dfb981a655998
ssm <44960143+nyussm@users.noreply.github.com>
2018-12-10 14:55:23 -0500

    Update trace_generator.py
    Add PacketNum in output file

--- trace_gen/trace_generator.py
+++ trace_gen/trace_generator.py
@@ -15,8 +15,9 @@ for flow in input:
     flow_length, arr_rand, size = tuple(map(int,input[flow][1:]))
     dep_adder = math.ceil(size/weight)   # int 1/weight ?
     output[flow] = [[] for i in range(flow_length)]   # initial list
-    output[flow][0] = [0, dep_adder, size]   #  first packet is special
+    output[flow][0] = [0, 0, dep_adder, size]   #  first packet is special
     for i in range(1, flow_length):
+        output[flow][i].append(i)
         output[flow][i].append(output[flow][i-1][0]+random.randint(0,arr_rand))     # arrive time
         output[flow][i].append((output[flow][i][0] \
                                 if output[flow][i][0] > output[flow][i-1][1] \
@@ -25,7 +26,7 @@ for flow in input:
 
 output_list = sorted(sorted(reduce(lambda x,y:x+y, \
                     [[[i]+j for j in output[i]] for i in output]), \
-                    key=lambda x:x[0]),key=lambda x:x[1])   # flownum first when departure time same
+                    key=lambda x:x[0]),key=lambda x:x[2])   # flownum first when departure time same
 
 with open(sys.argv[2], 'w') as f:
     for out in output_list:
