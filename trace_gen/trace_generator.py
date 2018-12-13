40455ff51d4e390dcac4af82ca358a64168555fd
ssm <44960143+nyussm@users.noreply.github.com>
2018-12-12 21:09:12 -0500

    Update trace_generator.py
    add counter to calculate total packet

--- trace_gen/trace_generator.py
+++ trace_gen/trace_generator.py
@@ -5,6 +5,8 @@ from functools import reduce
 
 input, output = {}, {}   # dictionary
 
+counter = 0
+
 with open(sys.argv[1], 'r') as f:
     for line in f:
         eles = line.split()
@@ -13,11 +15,12 @@ with open(sys.argv[1], 'r') as f:
 for flow in input:
     weight = float(input[flow][0])
     flow_length, arr_rand, size = tuple(map(int,input[flow][1:]))
+    counter = counter + int(input[flow][1])
     dep_adder = int(math.ceil(size/weight))   # int 1/weight ?
     output[flow] = [[] for i in range(flow_length)]   # initial list
     output[flow][0] = [0, 0, dep_adder, size]   #  first packet is special
     for i in range(1, flow_length):
-        output[flow][i].append(i)   #packet num
+        output[flow][i].append(i)
         output[flow][i].append(output[flow][i-1][0]+random.randint(0,arr_rand))     # arrive time
         output[flow][i].append((output[flow][i][0] \
                                 if output[flow][i][0] > output[flow][i-1][1] \
@@ -26,8 +29,9 @@ for flow in input:
 
 output_list = sorted(sorted(reduce(lambda x,y:x+y, \
                     [[[i]+j for j in output[i]] for i in output]), \
-                    key=lambda x:x[0]),key=lambda x:x[2])   # flownum first when arrive time same
+                    key=lambda x:x[0]),key=lambda x:x[2])   # flownum first when departure time same
 
 with open(sys.argv[2], 'w') as f:
+    f.write(str(counter) + '\n')
     for out in output_list:
         f.write(' '.join(list(map(str,out)))+'\n')
